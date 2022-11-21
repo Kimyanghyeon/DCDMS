05d286775b0eee0d4b745039a13088f4fa939cbc
KimYangHyeon <xiu9003260@gmail.com>
2022-11-21 21:52:12 +0900

    Modify form design

--- YangHyeon_Spring/src/main/java/kr/inhatc/spring/member/controller/memberController.java
+++ YangHyeon_Spring/src/main/java/kr/inhatc/spring/member/controller/memberController.java
@@ -1,6 +1,7 @@
 package kr.inhatc.spring.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,5 +17,11 @@ public class memberController {
 		return "member/memberLogin";
 
 	}// end of login
+
+	@GetMapping("/login/error")
+	public String loginError(Model model) {
+		model.addAttribute("loginErrorMsg", "아이디 또는 패스워드를 다시 확인하세요.");
+		return "member/memberLogin";
+	}// end of error
 
 }// end of class
